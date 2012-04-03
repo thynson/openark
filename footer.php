055e43b729062123a3bbedcced00b26cc3f598bb
Thynson <lanxingcan@gmail.com>
2012-04-03 17:35:00 +0800

    Add footer infos

--- footer.php
+++ footer.php
@@ -6,7 +6,8 @@
 </div><!-- End id=wrapper -->
 <div class="clear">&nbsp;</div>
 <div id="footnote" align="center">
-	Powered by <a href="http://wordpress.org/">Wordpress</a> and <a href="http://www.mysql.com/">MySQL</a>. Theme by <a href="http://code.openark.org/blog/shlomi-noach">Shlomi Noach</a>, <a href="http://openark.org">openark.org</a>, Improved for Chinese by <a href="http://thynson.me">Thynson</a>.
+	<p>Powered by <a href="http://wordpress.org/">Wordpress</a> and <a href="http://www.mysql.com/">MySQL</a>. Theme by <a href="http://code.openark.org/blog/shlomi-noach">Shlomi Noach</a>, <a href="http://openark.org">openark.org</a></p>
+	<p>Improved for Chinese by <a href="http://thynson.me">Thynson</a>, fork this themes at <a href="https://github.com/thynson/openark">github</a></p>
 </div>
 </div><!-- End align=center -->
 </body>
